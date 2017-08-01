0d09cf5558b948c91a448f3a9d728a26edd8a274
zamienASC4 <11toplelz@gmail.com>
2017-08-01 11:07:49 -0400

    userapi

---
+++ zamien-userapi/sketch.js
@@ -0,0 +1,18 @@
+var randomUserURL = "https://randomuser.me/api/";
+
+var userFirstName = "";
+var userLastName = "";
+var userPhoto = "";
+var userState = "";
+
+$.ajax({
+    url:randomUserURL,
+    success:function(data){
+        console.log(data.results[0]);
+        var userFirstName = data.results[0].name.first;
+        var userLastName = data.results[0].name.last;
+        var userPhoto = data.results[0].picture.thumbnail;
+        var userState = data.results[0].location.state;
+        $("body").append("<h1>This is "+userFirstName + " " +userLastName+" from "+ userState);
+    }
+});
