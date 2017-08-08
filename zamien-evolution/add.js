081541c632bdd90d79c76aee8a1f52f439ab5324
zamienASC4 <11toplelz@gmail.com>
2017-08-08 15:43:15 -0400

    project-evolution

---
+++ zamien-evolution/add.js
@@ -0,0 +1,21 @@
+//called when successful user log in
+function onSignIn(googleUser) {
+    console.log('User signed in!');
+    var profile = googleUser.getBasicProfile();
+    //change userName text, img source, & email text based on profile
+    $(".userName").text(profile.getName());
+    $("img").attr("src", profile.getImageUrl());
+    $(".email").text(profile.getEmail());
+}
+
+//called when "sign out" button clicked
+function onSignOut() {
+    //should sign user out and toggleHidden
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.')
+        //setting back to default
+        $(".userName").text("USER_NAME");
+        $("img").attr("src", "assets/placeholder.png");
+        $(".email").text("example@example.com");
+    });}
